3fd3146049cdf4b2e08505509d767a8f2212d73e
Jumraty Ahmad <jumratyahmad@MacBook-Air-Jumraty.local>
2025-10-17 13:59:02 +0700

    fix: add STR module in project and service model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
