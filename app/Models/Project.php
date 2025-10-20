fa1442876ccf830199f5c81a13845f1a8f83ff39
Jumraty Ahmad <jumratyahmad@MacBook-Air-Jumraty.local>
2025-10-20 22:52:40 +0700

    fix: fix remaining images return errror on project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
@@ -147,8 +148,6 @@ class Project extends Model implements HasMedia
 
     /**
      * Get a collection of all project images except the first one.
-     *
-     * @return \Illuminate\Support\Collection
      */
     public function getRemainingImages(): Collection
     {
